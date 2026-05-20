8217e98280dcc0465e495553c715faeb74e715ed
avika101 <avikashukla101@gmail.com>
2026-05-20 09:43:20 -0400

    initial commit

---
+++ src/HelloFX.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class HelloFX extends Application {
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("Tamagotchi coming soon :3");
+        Scene scene = new Scene(new StackPane(label), 400, 300);
+        stage.setScene(scene);
+        stage.setTitle("Tamagotchi");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
