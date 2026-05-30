e62b51b2165c544cdbe03047cb2adc5a137363fc
avika101 <avikashukla101@gmail.com>
2026-05-30 13:43:08 -0400

    token feature completed

--- src/HelloFX.java
+++ src/HelloFX.java
@@ -21,6 +21,8 @@ public class HelloFX extends Application {
     NeedsQueue needsQueue = new NeedsQueue();
     ActionStack actionStack = new ActionStack();
     ActionHistory actionHistory = new ActionHistory();
+    Shop shop = new Shop();
+    Label tokenLabel = new Label("Tokens: 0");
 
     //creating instances of progress bars
     ProgressBar hungerBar = new ProgressBar();
@@ -127,7 +129,7 @@ public class HelloFX extends Application {
         });
 
         VBox statBox = new VBox(5, hungerLabel, hungerBar, thirstLabel, thirstBar, happinessLabel,
-                happinessBar, energyLabel, energyBar); //label & bar container
+                happinessBar, energyLabel, energyBar, tokenLabel); //label & bar container
 
         HBox buttonBox = new HBox(10, feedBtn, playBtn, sleepBtn, treatBtn, waterBtn, undoBtn);
 // where the buttons will display
@@ -154,6 +156,7 @@ public class HelloFX extends Application {
             public void handle(ActionEvent actionEvent) {
                 pet.decayStats();
                 needsQueue.checkNeeds(pet);
+                shop.earnTokens(pet);
                 updateUI();
             }
         }));
@@ -174,6 +177,7 @@ public class HelloFX extends Application {
         happinessBar.setProgress(pet.getHappiness() / 100.0);
         energyBar.setProgress(pet.getEnergy() / 100.0);
         historyLabel.setText(actionHistory.getRecentActions(5));
+        tokenLabel.setText("Tokens: " + shop.getTokens());
 
         if (needsQueue.hasNeeds()) {
             statusLabel.setText("Your pet is " + needsQueue.getNextNeed() + "!");
--- src/Shop.java
+++ src/Shop.java
@@ -16,4 +16,55 @@ public class Shop {
         items.add(new ShopItem("Dreamscape", 100, "background"));
 
     }
+        public void earnTokens(Pet pet) {
+        int earned = 0;
+
+
+        if (pet.getHunger() >80) earned += 2;
+        if (pet.getThirst() >80) earned += 2;
+        if (pet.getEnergy() >80) earned += 2;
+        if (pet.getHappiness() > 80) earned += 2;
+        if (pet.getHunger() > 70 && pet.getHappiness() > 70
+                    && pet.getEnergy() > 70 && pet.getThirst() > 70) {
+                earned += 2;// streak bonus
+            }
+
+            tokens += earned;
+
+        }
+         public boolean buyItem(ShopItem item) {
+             if (tokens >= item.getCost() && !item.isOwned()) {
+                 tokens -= item.getCost();
+                 item.setOwned(true);
+                 return true; //purchase successful
+             } else {
+                 return false;
+             }
+
+         }
+             public ArrayList<ShopItem> getItems() { return items; }
+             public int getTokens() { return tokens; }
+
+             public ArrayList<ShopItem> getHats() {
+                 ArrayList<ShopItem> hats = new ArrayList<>();
+                 for (ShopItem item : items) {
+                     if (item.getType().equals("hat")) {
+                         hats.add(item);
+                     }
+                 }
+                 return hats;
+             }
+
+
+    public ArrayList<ShopItem> getBackgrounds() {
+        ArrayList<ShopItem> backgrounds = new ArrayList<>();
+        for (ShopItem item : items) {
+            if (item.getType().equals("background")) {
+                backgrounds.add(item);
+            }
+        }
+        return backgrounds;
+    }
 }
+
+
